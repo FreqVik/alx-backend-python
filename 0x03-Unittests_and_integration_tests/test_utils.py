e0ac13f63462ecff38285792b82eccd3f241d351
VikingVik123 <vodimmegwa@gmail.com>
2025-05-29 13:46:13 +0100

    fix/modded utils test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,6 +69,9 @@ class TestMemoize(unittest.TestCase):
         """
 
         class TestClass:
+            """
+            A test class to demonstrate memoization.
+            """
             def a_method(self):
                 return 42
 
@@ -76,7 +79,9 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42)as mock_method:
+        with patch.object(
+            TestClass, 'a_method', return_value=42
+        ) as mock_method:
             obj = TestClass()
             self.assertEqual(obj.a_property, 42)
             self.assertEqual(obj.a_property, 42)
