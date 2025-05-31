fe1bf8d9f1d3607b04299f97fc34374d39d15284
VikingVik123 <vodimmegwa@gmail.com>
2025-05-31 10:10:32 +0100

    fix/updated urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # Just include the app-level router
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
