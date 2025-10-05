5f6379d4e9443cd32b2475e8406d883a9454bffc
VikingVik123 <vodimmegwa@gmail.com>
2025-10-05 23:30:41 +0100

    fix/modded task 2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 
@@ -31,3 +32,15 @@ def log_message_edit(sender, instance, **kwargs):
                 edited_by=instance.edited_by
             )
             instance.edited = True
+
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    """
+    After a user is deleted, remove all related messages,
+    notifications, and message histories.
+    """
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(message__sender=instance).delete()
